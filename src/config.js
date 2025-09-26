8174f3d6704103a004a0f436770bd75fa391b53d
Tabigat Karbayev <tab1k@MacBook-Pro-Tabigat.local>
2025-09-27 01:27:21 +0500

    add

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-// export const API_URL = 'http://85.116.187.153:8000/api/home';
-// export const MEDIA_API_URL = 'http://85.116.187.153:8000';
-// export const LOGIN_URL = 'http://85.116.187.153:8000';
+export const API_URL = 'http://85.116.187.153:8000/api/home';
+export const MEDIA_API_URL = 'http://85.116.187.153:8000';
+export const LOGIN_URL = 'http://85.116.187.153:8000';
 
-export const API_URL = 'http://172.20.10.3:8000/api/home';
-export const MEDIA_API_URL = 'http://172.20.10.3:8000';
-export const LOGIN_URL = 'http://172.20.10.3:8000';
+// export const API_URL = 'http://172.20.10.3:8000/api/home';
+// export const MEDIA_API_URL = 'http://172.20.10.3:8000';
+// export const LOGIN_URL = 'http://172.20.10.3:8000';
 
 
